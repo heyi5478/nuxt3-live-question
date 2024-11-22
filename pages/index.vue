57a96eabf728e8bf36cced02c1fa295e4bf6d2a3
YI-JYUN HE <jobs.steve54@gmail.com>
2024-11-22 13:45:54 +0800

    day2 任務

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Page: 首頁
+  <div>Page: 首頁</div>
+  <div class="">
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
